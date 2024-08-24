a0cf467ee7b39242103ee91dbbba955574533df6
Akhil <144369179+AkhilRai28@users.noreply.github.com>
2024-08-24 21:24:06 +0530

    Patch1

--- Single Port RAM.srcs/sources_1/new/module_design.v
+++ Single Port RAM.srcs/sources_1/new/module_design.v
@@ -4,43 +4,247 @@
 // Engineer: 
 // 
 // Create Date: 30.03.2024 17:08:27
-// Design Name: 
-// Module Name: module_design
+// Design Name: Single Port RAM
+// Module Name: single_port_ram
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: Single Port RAM module with synchronous write and asynchronous read.
 // 
-// Dependencies: 
+// Dependencies: None
 // 
 // Revision:
 // Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Additional Comments: 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
-
-// Single Port RAM module design
-
-module module_design(
-  input [7:0] data, //input data
-  input [5:0] addr, //address
-  input we, //write enable
-  input clk, //clk
-  output [7:0] q //output data
+module single_port_ram(
+    input [7:0] data,    // 8-bit input data
+    input [7:0] addr,    // 8-bit address (increased to 256 locations)
+    input we,            // Write enable signal
+    input clk,           // Clock signal
+    output reg [7:0] q   // 8-bit output data
 );
-  
-  reg [7:0] ram [63:0]; //8*64 bit ram
-  reg [5:0] addr_reg; //address register
- 
-  always @ (posedge clk)
-    begin
-      if(we)
-        ram[addr] <= data;
-      else
-        addr_reg <= addr; 
+    
+    // Declare a 256x8-bit RAM
+    reg [7:0] ram [255:0];
+    
+    // Address register to hold the read address for asynchronous read
+    reg [7:0] addr_reg;
+    
+    // Synchronous write and address capture
+    always @(posedge clk) begin
+        if (we) begin
+            ram[addr] <= data;  // Write data to the memory location at addr
+        end
+        addr_reg <= addr;       // Update the address register with the current address
     end
- 
-  assign q = ram[addr_reg];
-  
+    
+    // Asynchronous read (output based on the registered address)
+    always @(*) begin
+        q = ram[addr_reg];      // Output the data from the memory location stored in addr_reg
+    end
+    
 endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//comment
